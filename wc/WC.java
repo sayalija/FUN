38090c70d0c40393f4a281fd1fa5c097c95324ac
sayali <sayali@patanaikya.com>
2014-01-04 13:13:26 +0530

    refactored with multiple methods

--- wc/WC.java
+++ wc/WC.java
@@ -2,12 +2,19 @@ public class WC{
 	public int countChars(String text){
 		return text.length();
 	}
+
+	public int countWords(String text){
+		return text.split(" ").length;
+	}
+
+	public int countLines(String text){
+		return text.split("\n").length;
+	}
+
 	public static void main(String[] args) {
 		String text = "My name is Sayalli\n I am form Pune\n I am working as intern in TW.";
 		int charCount = 0,wordCount = 0,lineCount = 0;
 		WC wc = new WC(	);
-		System.out.println("Characters = " + wc.countChars(text));
-		System.out.println("Words = " + text.split(" ").length);
-		System.out.println("Lines = " + text.split("\n").length);
+		System.out.println( wc.countLines(text)+"\t"+wc.countWords(text)+"\t"+wc.countChars(text) + "    abc.txt");
 	}
 }

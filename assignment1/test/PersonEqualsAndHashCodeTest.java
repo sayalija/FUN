7d410672fb1165b41eb6aeaf4cabf25bd22b51a3
unknown <sayalija@Insayalija.corporate.thoughtworks.com>
2014-01-14 12:10:32 +0530

    WrongPerson test cases

--- assignment1/test/PersonEqualsAndHashCodeTest.java
+++ assignment1/test/PersonEqualsAndHashCodeTest.java
@@ -37,4 +37,27 @@ public class PersonEqualsAndHashCodeTest {
         Assert.assertEquals(false, Swamiji.equals(Kunal));
     }
 
+
+    @Test
+    public void testHashCodeWillReturnTrueForSameWrongPersonTwoTimes() throws Exception {
+        WrongPerson Swamiji = new WrongPerson("Swamiji", 34);
+        int hash1 = Swamiji.hashCode();
+        int hash2 = Swamiji.hashCode();
+        Assert.assertEquals(false, hash1 == hash2);
+    }
+
+    @Test
+    public void testEqualsReturnTrueForSameWrongPerson() throws Exception {
+        WrongPerson p1 = new WrongPerson("Swamiji", 34);
+        WrongPerson p2 = new WrongPerson("Swamiji", 34);
+        Assert.assertEquals(false, p1.equals(p2));
+    }
+
+    @Test
+    public void testEqualsReturnFalseForDifferentWrongPerson() throws Exception {
+        WrongPerson Swamiji = new WrongPerson("Swamiji", 34);
+        WrongPerson Kunal = new WrongPerson("Kunal", 25);
+        Assert.assertEquals(true, Swamiji.equals(Kunal));
+    }
+
 }
